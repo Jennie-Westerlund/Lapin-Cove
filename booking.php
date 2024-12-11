629020c7eb5afa1cbcbc728839d005648022a7c5
Jennie-Westerlund <jmn.westerlund@gmail.com>
2024-12-11 12:06:37 +0100

    Connected PHP to database

--- booking.php
+++ booking.php
@@ -4,9 +4,18 @@ declare(strict_types=1);
 
 header(__DIR__ . '/index.html');
 
+$database = new PDO('sqlite:' . __DIR__ . '/Backend/test.db');
+
 if (isset($_POST['startDate']) && ($_POST['endDate'])) {
     $startDate = $_POST['startDate'];
     $endDate = $_POST['endDate'];
     $room = $_POST['Room'];
     echo "$startDate and $endDate in the $room room";
 }
+
+
+$statement = $database->query('SELECT * FROM Rooms;');
+
+$roomsData = $statement->fetch(PDO::FETCH_ASSOC);
+
+echo $roomsData['Room'];
